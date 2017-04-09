58b79ffb9b03f13587674c78e3311a802a7ce0f0
alexstar2008 <alexstar2008@ukr.net>
2017-04-09 17:52:51 +0300

    Start_commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 'use strict';
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('default',function(){
-  console.log('all is ok');
+gulp.task('default',['sass','sass:watch']);
+gulp.task('sass',function(){
+  gulp.src('src/styles/sass/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('built/css'));
+});
+gulp.task('sass:watch', function () {
+  gulp.watch('src/styles/sass/**/*.scss', ['sass']);
 });
