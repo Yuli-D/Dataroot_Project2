0299caf1e992a6eaee44a4cd61456fec5e5a3ad5
Yuliia Dehtiarova <yulidehtiarova@gmail.com>
2017-04-10 22:32:03 +0300

    config

--- css/Gulpfile.js
+++ css/Gulpfile.js
@@ -1,13 +1,12 @@
 var gulp=require('gulp');
 var sass=require('gulp-sass');
 var watch = require('gulp-watch');
-gulp.task('styles',function()
+gulp.task('sass',function()
 		  {
 	gulp.src('../sass/**/*.scss')
 	.pipe(sass().on('error',sass.logError))
 	.pipe(gulp.dest(''))
-          });
-gulp.task('default',['sass','sass:watch']); 
-gulp.task('sass:watch', function () { 
-gulp.watch('../sass/**/*.scss', ['sass']); 
+          }); 
+gulp.task('sass:watch', function () {
+  gulp.watch('../sass/**/*.scss', ['sass']);
 });
