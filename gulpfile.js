03f59ee060f374ed7e400da75f5e4c69a654e7a0
Yuliia Dehtiarova <yulidehtiarova@gmail.com>
2017-04-16 23:20:52 +0300

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-'use strict';
-const gulp = require('gulp');
-
-gulp.task('default',function(){
-  console.log('all is ok');
+var gulp=require('gulp');
+var sass=require('gulp-sass');
+var watch = require('gulp-watch');
+gulp.task('sass',function()
+		  {
+	gulp.src('sass/**/*.scss')
+	.pipe(sass().on('error',sass.logError))
+	.pipe(gulp.dest('css'))
+          }); 
+gulp.task('sass:watch', function () {
+  gulp.watch('sass/**/*.scss', ['sass']);
 });
